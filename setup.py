e5590257584a5686f6029fdddfc73e4cba11e782
PrathamBhat-prog <prathambhat75@gmail.com>
2025-03-25 19:11:41 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author= 'PrathamBhat-prog',
+    author_email='prathambhat75@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+
+)
